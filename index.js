cd580e2f71fb9432a94eab9e159a084872f138e4
delbyte <blazed0utcode@gmail.com>
2025-02-27 23:15:03 +0530

    playing around with adding routes

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/route1', (req, res) => {
+  res.send('Hello World from another route!')
+})
+
+app.get('/route2', (req, res) => {
+  res.send(<html> <body> <h1>Chat</h1> <h2>Here's an html request being sent from route2. </h2></body></html>)
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
